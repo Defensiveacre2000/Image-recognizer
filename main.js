eb4734ce57eabdd2a9852de55093f559818b9ec0
Defensiveacre2000 <86143931+Defensiveacre2000@users.noreply.github.com>
2021-07-22 14:59:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+width : 350,
+height : 300,
+image_format:'png',
+png_quality:90
+});
+camera = document.getElementById("Cam");
+Webcam.attach('#Cam');
+function snap()
+{
+Webcam.snap(function(data_uri)
+{
+document.getElementById("result").innerHTML = '<img id="capture" src="' + data_uri + '">';
+});
+}
+console.log('ml5 version : ',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/pHrFurPk9/model.json',modelLoaded);
+function modelLoaded()
+{
+console.log('modelLoaded');
+}
+function check()
+{
+img = document.getElementById('captured_image');
+classifier.classify(img , gotResult);
+}
+function gotResult(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+else
+{
+console.log(results);
+document.getElementById("ron").innerHTML = results[0].label;
+document.getElementById("roa").innerHTML = results[1].confidence.toFixed(3);
+}
+}
